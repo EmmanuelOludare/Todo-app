5bfd83476c2ca89012fe287bde3587872f2c3d0c
Michaelhybrid <michaeloludare15@gmail.com>
2023-03-27 22:51:46 +0100

    Todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from 'react';
 import addNotification from 'react-push-notification';
-import { Notifications } from 'react-push-notification';
+import removeNotification from 'react-push-notification';
 import moon from '../assets/images/icon-moon.svg';
 import sun from '../assets/images/icon-sun.svg';
 import check from '../assets/images/icon-check.svg';
@@ -23,12 +23,23 @@ const Todo = (props) => {
   /*set tasks to local storage*/
   const addTask = e => {
     e.preventDefault();
+    if (e.key === "Enter") {
       const newTask = { text: taskText, completed: false };
         const updatedTasks = [...tasks, newTask];
         const reversedArray = [...updatedTasks].reverse();
         setTasks(reversedArray);
         localStorage.setItem('tasks', JSON.stringify(reversedArray));
         setTaskText('');
+        Notification.requestPermission(function() {
+          if (Notification.permission === 'granted') {
+             setNotificationStatus('Permission granted');
+          } else if (Notification.permission === 'denied') {
+            setNotificationStatus('Permission denied');
+          } else {
+            setNotificationStatus('Permission default');
+          }
+      });
+    }
   };
 
   /*get tasks from local storage*/
@@ -99,54 +110,63 @@ const Todo = (props) => {
     localStorage.setItem('tasks', JSON.stringify(rearrangedTasks));
   }
 
-  function buttonOnClick (){
-    addNotification({
-      title: 'Warning',
-      native:true         
-    })
-  };
+  const handleNotifications = () => {
+    if(Notification.permission === 'granted') {
+      if (document.hidden) {
+        removeNotification({
+            title: 'Pending Tasks',
+            message: `You have pending tasks!`,
+            native: true,        
+          });
+        }
+    }
+  }
+
+  useEffect(() => {
+      document.addEventListener('visibilitychange',handleNotifications);
+      return () => document.removeEventListener('visibilitychange',handleNotifications);
+  },[]);
 
   return (
     <div className=''>
-      <Notifications />
-        <div className="bg-mobile-light-mode bg-cover bg-center bg-no-repeat w-screen h-44 flex flex-col gap-3 pt-8 px-8 dark:bg-mobile-dark-mode">
-          <div className='flex justify-between'>
-              <h3 className="text-very-light-gray text-3xl tracking-[.5rem] font-bold">TODO</h3>
-              <img className='w-6 h-6 cursor-pointer' src={props.darkMode ? sun : moon} alt="light-dark-mode" onClick={() => props.handleTheme(!props.darkMode)} />
-          </div>
-          <div className="flex flex-col">
-            <div className='border-dark-grayish-blue border-2 rounded-full w-6 h-6 p-2 absolute mt-3 ml-3.5 opacity-40'>{/*<img className='w-3 h-3 m-auto' src={check} alt="light-dark-mode"/> bg-gradient-to-r from-from to-to*/}</div>
-            <input type="text" aria-label='Create a new todo...' placeholder='Create a new todo...' className='indent-12 rounded-lg py-3.5 dark:bg-very-dark-desaturated-blue' value={taskText} onChange={handleTaskText} />
-            <img className='w-4 h-4 cursor-pointer absolute mt-3.5 right-12 rotate-45' src={cross} alt="cancel" onClick={addTask}/>
-          </div> 
-        </div>
-        <div className="flex flex-col bg-white shadow-md rounded-lg mx-8 mt-[-30px] dark:bg-very-dark-desaturated-blue">
-          {tasksCategory[taskIndex] === 0 ? <p className='text-center text-xl'>{tasksCategoryText[taskIndex]}</p> : tasksCategory[taskIndex].map((task, index) => (
-              <div 
-                className="flex justify-between items-center border-b-[.5px] border-dark-grayish-blue p-3 cursor-pointer" 
-                key={index}
-                draggable={true}
-                onDragStart={(event) => handleDragStart(event, index)}
-                onDragOver={handleDragOver}
-                onDrop={(event) => handleDrop(event, index)}>
-                <div onClick={() => completeTask(index)} className={`border-dark-grayish-blue border-2 rounded-full w-8 h-8 p-2 ${task.completed ? "bg-gradient-to-r from-from to-to" : "bg-none"}`}><img className={`w-4 h-3 m-auto ${task.completed ? "flex" : "hidden"}`} src={check} alt="Completed"/></div>
-                <p className={`text-md mr-auto ml-3 ${task.completed ? "line-through text-dark-grayish-blue dark:text-dark-grayish-blue" : "no-underline text-black dark:text-light-grayish-blue(hover)"}`}>{task.text}</p>
-                <img className='w-4 h-4 cursor-pointer' src={cross} alt="cancel" onClick={() => deleteTask(index)}/>
-              </div>
-            ))}
-            <div className="flex justify-between py-5 px-3">
-              <p className="text-sm text-dark-grayish-blue">{tasksCategory[taskIndex].length} items left</p>
-              <p onClick={clearCompletedTasks} className="text-sm text-dark-grayish-blue">Clear Completed</p>
+        <div className="bg-mobile-light-mode bg-cover bg-center bg-no-repeat w-screen h-48 px-8 pt-8 dark:bg-mobile-dark-mode md:bg-desktop-light-mode md:h-56 md:dark:bg-mobile-dark-mode md:px-[16vw] lg:px-[25vw] xl:px-[30vw]">
+          <div className="flex flex-col gap-3 md:gap-7 max-w-[600px]">
+            <div className='flex justify-between w-full'>
+                <h3 className="text-very-light-gray text-3xl tracking-[.5rem] font-bold">TODO</h3>
+                <img className='w-6 h-6 cursor-pointer' src={props.darkMode ? sun : moon} alt="light-dark-mode" onClick={() => props.handleTheme(!props.darkMode)} />
             </div>
+            <div className="flex flex-col w-full">
+              <div className='border-dark-grayish-blue border-2 rounded-full w-6 h-6 p-2 absolute mt-3 ml-3.5 opacity-40'></div>
+              <input type="text" aria-label='Create a new todo...' placeholder='Create a new todo...' className='indent-12 rounded-lg py-3.5 dark:bg-very-dark-desaturated-blue dark:text-light-grayish-blue(hover)' value={taskText} onChange={handleTaskText} onKeyUp={addTask}/>
+            </div> 
+          </div>
         </div>
-        <div className="flex gap-8 justify-center bg-white-700 shadow-md rounded-lg mx-8 py-3.5  mt-5">
-          <p onClick={displayAllTasks} className={`text-lg font-semibold cursor-pointer ${taskIndex === 0 ? 'text-bright-blue' : 'text-dark-grayish-blue'}`}>All</p>
-          <p onClick={displayActiveTasks} className={`text-lg font-semibold cursor-pointer ${taskIndex === 1 ? 'text-bright-blue' : 'text-dark-grayish-blue'}`}>Active</p>
-          <p onClick={displayCompletedTasks} className={`text-lg font-semibold cursor-pointer ${taskIndex === 2 ? 'text-bright-blue' : 'text-dark-grayish-blue'}`}>Completed</p>
-        </div>
-        <p className="text-dark-grayish-blue text-center mt-10">Drag and drop to reorder list</p>
-        <button onClick={buttonOnClick}>Push Notification</button>
-    </div>
+          <div className="max-w-[600px] flex flex-col bg-white shadow-md rounded-lg mx-8 mt-[-30px] dark:bg-very-dark-desaturated-blue md:mx-[16vw] lg:mx-[25vw] xl:mx-[30vw] md:mt-[-45px] ">
+            {tasksCategory[taskIndex] === 0 ? <p className='text-center text-xl'>{tasksCategoryText[taskIndex]}</p> : tasksCategory[taskIndex].map((task, index) => (
+                <div 
+                  className="flex justify-between items-center border-b-[.5px] border-dark-grayish-blue p-3 cursor-pointer" 
+                  key={index}
+                  draggable={true}
+                  onDragStart={(event) => handleDragStart(event, index)}
+                  onDragOver={handleDragOver}
+                  onDrop={(event) => handleDrop(event, index)}>
+                  <div onClick={() => completeTask(index)} className={`border-dark-grayish-blue border-2 rounded-full w-8 h-8 p-2 ${task.completed ? "bg-gradient-to-r from-from to-to" : "bg-none"}`}><img className={`w-4 h-3 m-auto ${task.completed ? "flex" : "hidden"}`} src={check} alt="Completed"/></div>
+                  <p className={`text-md mr-auto ml-3 ${task.completed ? "line-through text-dark-grayish-blue dark:text-dark-grayish-blue" : "no-underline text-black dark:text-light-grayish-blue(hover)"}`}>{task.text}</p>
+                  <img className='w-4 h-4 cursor-pointer' src={cross} alt="cancel" onClick={() => deleteTask(index)}/>
+                </div>
+              ))}
+              <div className="flex justify-between py-5 px-3">
+                <p className="text-sm text-dark-grayish-blue">{tasksCategory[taskIndex].length} items left</p>
+                <div className="max-w-[600px] flex justify-center gap-6 mt-[55px] absolute left-0 right-0 bg-white shadow-md rounded-lg mx-8 py-2 dark:bg-very-dark-desaturated-blue md:mt-0 md:relative md:shadow-none md:rounded-none md:mx-0 md:py-0">
+                  <p onClick={displayAllTasks} className={`text-lg font-semibold cursor-pointer ${taskIndex === 0 ? 'text-bright-blue' : 'text-dark-grayish-blue'}`}>All</p>
+                  <p onClick={displayActiveTasks} className={`text-lg font-semibold cursor-pointer ${taskIndex === 1 ? 'text-bright-blue' : 'text-dark-grayish-blue'}`}>Active</p>
+                  <p onClick={displayCompletedTasks} className={`text-lg font-semibold cursor-pointer ${taskIndex === 2 ? 'text-bright-blue' : 'text-dark-grayish-blue'}`}>Completed</p>
+                </div>
+                <p onClick={clearCompletedTasks} className="text-sm text-dark-grayish-blue cursor-pointer">Clear Completed</p>
+              </div>
+          </div>
+          <p className="text-dark-grayish-blue text-center mt-[75px] md:mt-[20px]">Drag and drop to reorder list</p>
+      </div>
   )
 }
 
